083b6b8008ce4ceeba64bd3aa2ec2d34283483a9
Dharshini-S12 <118833017+Dharshini-S12@users.noreply.github.com>
2024-04-28 18:40:00 +0530

    Commit

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ const button = document.getElementById('btn')
 const updateButton = document.getElementById('upbtn')
 const table = document.getElementById('table')
 const dataButton = document.getElementById('getdata')
-const API_URL = "http://localhost:3000/users" // Local JSON server url running at port 3000 run the server by 'npx json-server -p 3000 db.json'
+const API_URL = "http://localhost:3000/users"
 var uid
 
 
 button.addEventListener('click', () => {
-    const id = Math.floor(Math.random()*100) // Giving random id to the user using random function
+    const id = Math.floor(Math.random()*100) 
     const users = {
         id: `${id}`,
         fname: `${myFName.value}`,
         lname: `${myLName.value}`,
         phone: `${myphone.value}`
     }
-    handlePost(users) // Calling post function to post the users at the json server 
+    handlePost(users) 
 })
 
 dataButton.addEventListener('click', async () => {
-    const data = await handleGet() // Calling get function to get the users from the json server and it takes time to convert the data to json so await is used
-    data.forEach( e => { // data is an array so forEach is used for all the users stored in the array
+    const data = await handleGet() 
+    data.forEach( e => { 
         const newRow = `
                             <div class="section-right-contacts">
                                 <div class="contact-1">
@@ -74,10 +74,10 @@ const handleGet = async () => {
         const response = await fetch(API_URL, {
             method:'GET',
             headers: {
-                'Accept': 'Application/json' // Accepting the response as json
+                'Accept': 'Application/json'
             }
         }) 
-        const data = await response.json() // converting the response into json it takes time so await is used. If await is not used it will return a promise with state pending
+        const data = await response.json()
         return(data)
     }
     catch(err)
@@ -92,9 +92,9 @@ const handlePost = async(users) => {
         const response = await fetch(API_URL,{
             method:'POST',
             headers: {
-                'Content-Type' : 'Application/json' // posting the data into json server so the content type is mentioned as json 
+                'Content-Type' : 'Application/json'  
             },
-            body: JSON.stringify(users) // converting the object user into json using stringify method. the content inside the body will be posted at the server
+            body: JSON.stringify(users) 
         })
     }
     catch(err)
@@ -106,7 +106,7 @@ const handlePost = async(users) => {
 const handleDelete = async (id) => {
     try
     {
-        const response = await fetch(`${API_URL}/${id}`, { // The url of the json server is constructed again using the id that of the user wanted to be deleted 
+        const response = await fetch(`${API_URL}/${id}`, {
             method: 'DELETE'
         })
     }
@@ -143,7 +143,7 @@ updateButton.addEventListener('click', (e) => {
 const handleJsonEdit = async (users) => {
     try
     {
-        const response = await fetch(`${API_URL}/${uid}`,{ // The url of the json server is constructed again using the id that of the user wanted to be updated
+        const response = await fetch(`${API_URL}/${uid}`,{ 
             method: 'PUT',
             headers: {
                 'Content-Type' : 'Application/json'
